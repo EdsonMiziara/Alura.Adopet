d6c8208981a5c8f5a8b71d7670fa98de8f27ed2f
EdsonMiziara <edselmizi@gmail.com>
2025-06-22 19:04:10 -0300

    #Update: Extação de Classes

---
+++ Alura.Adopet.Console/Apoio/Import.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+
+namespace Alura.Adopet.Console.Apoio;
+
+public class Import
+{
+
+    public readonly Extension extension;
+    HttpClient client;
+
+    public Import()
+    {
+        extension = new Extension();
+        this.client = extension.client;
+    }
+    public async void ImportacaoArquivoPet(string caminhoDoArquivoDeImportacao)
+    {
+        List<Pet> listaDePet = new List<Pet>();
+
+        string caminhoDoArquivoASerImportado = caminhoDoArquivoDeImportacao;
+        using (StreamReader sr = new StreamReader(caminhoDoArquivoASerImportado))
+        {
+            while (!sr.EndOfStream)
+            {
+                // separa linha usando ponto e vírgula
+                string[] propriedades = sr.ReadLine().Split(';');
+                // cria objeto Pet a partir da separação
+                Pet pet = new Pet(Guid.Parse(propriedades[0]),
+                  propriedades[1],
+                  TipoPet.Cachorro
+                 );
+
+                System.Console.WriteLine(pet);
+                listaDePet.Add(pet);
+            }
+        }
+        foreach (var pet in listaDePet)
+        {
+            try
+            {
+                var resposta = await extension.CreatePetAsync(pet);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+        System.Console.WriteLine("Importação concluída!");
+    }
+}
---
+++ Alura.Adopet.Console/Apoio/List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console.Apoio;
+
+public class List
+{
+    Extension extension;
+
+    public List()
+    {
+        extension = new Extension();
+    }
+
+    public async void ListPetsAsync()
+    {
+        var pets = await extension.ListPetsAsync();
+        foreach (var pet in pets)
+        {
+            System.Console.WriteLine(pet);
+        }
+    }
+}
