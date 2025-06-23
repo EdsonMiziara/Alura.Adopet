e93d1941fc05f271eb6d4f8f5dfac41cf39cfb6c
EdsonMiziara <edselmizi@gmail.com>
2025-06-23 15:46:29 -0300

    #Update: Encapsulando dos comandos em uma classe.

---
+++ Alura.Adopet.Console/Comandos/ComandosDoSistema.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console.Comandos;
+
+internal class ComandosDoSistema
+{
+    private Dictionary<string, IComando> comandosDoSistema = new()
+    {
+        { "import", new Import() },
+        { "help", new Help() },
+        { "show", new Show() },
+        { "list", new List() }
+    };
+
+    public IComando? this [string key] =>
+        comandosDoSistema.ContainsKey(key) ? comandosDoSistema[key] : null;
+}
--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -1,36 +1,24 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using Alura.Adopet.Console.Comandos;
 using Alura.Adopet.Console.Modelos;
 
-// na linha abaixo cria-se uma instância de HttpClient para consumir API Adopet.
-HttpClient client = ConfiguraHttpClient("http://localhost:5057");
+ComandosDoSistema comandos = new();
 Console.ForegroundColor = ConsoleColor.Green;
 try
-{    
+{
     string comando = args[0].Trim();
-    switch (comando)
+    var cmd = comandos[comando];
+    if (cmd is not null )
     {
-        case "import":
-            var import = new Import();
-            await import.ExecutarAsync(args);
-            break;
-        case "help":     
-            var help = new Help();
-            await help.ExecutarAsync(args);
-            break;
-        case "show":  
-            var show = new Show();
-            await show.ExecutarAsync(args);
-            break;
-        case "list":
-            var list = new List();
-            await list.ExecutarAsync(args);
-            break;
-        default:
-            Console.WriteLine("Comando inválido!");
-            break;
+        await cmd.ExecutarAsync(args);
     }
+    else
+    {
+        Console.WriteLine($"Comando '{comando}' não encontrado. Use 'adopet help' para ver os comandos disponíveis.");
+    }
+
 }
 catch (Exception ex)
 {
@@ -41,27 +29,4 @@ catch (Exception ex)
 finally
 {
     Console.ForegroundColor = ConsoleColor.White;
-}
-
-HttpClient ConfiguraHttpClient(string url)
-{
-    HttpClient _client = new HttpClient();
-    _client.DefaultRequestHeaders.Accept.Clear();
-    _client.DefaultRequestHeaders.Accept.Add(
-        new MediaTypeWithQualityHeaderValue("application/json"));
-    _client.BaseAddress = new Uri(url);
-    return _client;
-}
-Task<HttpResponseMessage> CreatePetAsync(Pet pet)
-{
-    HttpResponseMessage? response = null;
-    using (response = new HttpResponseMessage())
-    {
-        return client.PostAsJsonAsync("pet/add", pet);
-    }
-}
-async Task<IEnumerable<Pet>?> ListPetsAsync()
-{
-    HttpResponseMessage response = await client.GetAsync("pet/list");
-    return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
 }
